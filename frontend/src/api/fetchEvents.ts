6c419abc07e92c12183d30ac2585e06ec2ad9f9d
Pratik Singh <pratikthakur4761@gmail.com>
2026-03-27 14:52:58 +0530

    Fixed.

--- frontend/src/api/fetchEvents.ts
+++ frontend/src/api/fetchEvents.ts
@@ -1,6 +1,6 @@
 // Fetch all events
 export async function fetchEvents() {
-  const url = 'http://localhost:3000/events';
+  const url = 'https://cems-e5eo.onrender.com/events';
   const response = await fetch(url, {
     method: 'GET',
     headers: {
@@ -15,7 +15,7 @@ export async function fetchEvents() {
 //fetch token for login.
 export async function getToken(email: string, password: string) {
 
-  const url = 'http://localhost:3000/users/login';
+  const url = 'https://cems-e5eo.onrender.com/users/login';
   const response = await fetch(url, {
     method: 'POST',
     headers: {
@@ -33,7 +33,7 @@ export async function getToken(email: string, password: string) {
 //fetch events for specific user.
 export async function fetchMyEvents() {
 
-  const url = 'http://localhost:3000/my-events';
+  const url = 'https://cems-e5eo.onrender.com/my-events';
   const token = localStorage.getItem('token');
   if (!token) {
     throw new Error('No token found');
@@ -53,7 +53,7 @@ export async function fetchMyEvents() {
 
 //register event for specific user.
 export async function registerEvent(event_id: number) {
-  const url = `http://localhost:3000/events/${event_id}/register`;
+  const url = `https://cems-e5eo.onrender.com/events/${event_id}/register`;
   const token = localStorage.getItem('token');
   if (!token) {
     throw new Error('No token found');
@@ -70,7 +70,7 @@ export async function registerEvent(event_id: number) {
 }
 
 export async function createEvent(title: string, description: string, event_time: string) {
-  const url = 'http://localhost:3000/events';
+  const url = 'https://cems-e5eo.onrender.com/events';
   const token = localStorage.getItem('token');
   if (!token) {
     throw new Error('No token found');
